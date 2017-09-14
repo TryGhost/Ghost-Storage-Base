e0b1a30a7dbe0271a8a33dc82b8dd1e492f864e0
kirrg001 <katharina.irrgang@googlemail.com>
2017-09-14 17:10:30 +0200

    Addes tests for getUniqueFileName fn
    no issue
    
    - added bluebird to dev dependencies
    - added some tests to ensure getUniqueFileName works as expected

--- test/BaseStorage_spec.js
+++ test/BaseStorage_spec.js
@@ -1,9 +1,69 @@
 var should = require('should'),
+    Promise = require('bluebird'),
     StorageBase = require('../BaseStorage');
 
 describe('Storage Base', function () {
-    it('escape non accepted characters in filenames', function () {
+    it('getSanitizedFileName: escape non accepted characters in filenames', function () {
         var storage = new StorageBase();
         storage.getSanitizedFileName('(abc*@#123).zip').should.eql('-abc-@-123-.zip');
+    });
+
+    it('getUniqueFileName: accepts jpg', function (done) {
+        var storage = new StorageBase(),
+            i = 0;
+
+        storage.exists = function () {
+            i = i + 1;
+
+            if (i < 2) {
+                return Promise.resolve(true);
+            } else {
+                return Promise.resolve(false);
+            }
+        };
+
+        storage.getUniqueFileName({name: 'something.jpg'}, 'target-dir')
+            .then(function (filename) {
+                filename.should.eql('target-dir/something-1.jpg');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('getUniqueFileName: accepts png', function (done) {
+        var storage = new StorageBase();
+
+        storage.exists = function () {
+            return Promise.resolve(false);
+        };
+
+        storage.getUniqueFileName({name: 'something.png'}, 'target-dir')
+            .then(function (filename) {
+                filename.should.eql('target-dir/something.png');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('getUniqueFileName: deny .1 extension', function (done) {
+        var storage = new StorageBase(),
+            i = 0;
+
+        storage.exists = function () {
+            i = i + 1;
+
+            if (i < 2) {
+                return Promise.resolve(true);
+            } else {
+                return Promise.resolve(false);
+            }
+        };
+
+        storage.getUniqueFileName({name: 'something.1'}, 'target-dir')
+            .then(function (filename) {
+                filename.should.eql('target-dir/something.1-1');
+                done();
+            })
+            .catch(done);
     });
 });
