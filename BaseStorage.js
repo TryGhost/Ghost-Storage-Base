f8a281d57f1a7e9ece7af162e3a4b6fbc72568d7
Sag <guptazy@gmail.com>
2025-01-16 16:28:49 +0700

    🔒 Improved filename generation for uniqueness, security, and length constraints
    ref https://linear.app/ghost/issue/ENG-1260
    ref https://linear.app/ghost/issue/ENG-1859/gluster-file-name-length-error-on-uploaded-media
    
    - filename are now generated using a 16-character hash, that gets re-generated on every upload
    - we do not check whether the file exists anymore, as a change of collission is near zero below a million files in a directory
    - if the filename length is higher than what most filesystem accept (255 bytes), we truncate the stem of the filename, while keeping space for the unique hash, the eventual original suffix "_o" and the file extension
    - example:
        - input: "long-name-for-an-image-imagine-255bytes-followed-by-original-suffix_o.png"
        - output: "long-name-for-an-image-1a2b3c4d5e6f7g8h9i_o.png"

--- BaseStorage.js
+++ BaseStorage.js
@@ -1,5 +1,12 @@
-const moment = require('moment'),
-    path = require('path');
+const moment = require('moment');
+const path = require('path');
+const crypto = require('crypto');
+
+// Filesystems usually have a filename length limit -- 255 bytes is the standard limit on Unix-based systems
+const MAX_FILENAME_BYTES = 255;
+
+// When an uploaded file is transformed, we keep the original file with the '_o suffix
+const ORIGINAL_SUFFIX = '_o';
 
 class StorageBase {
     constructor() {
@@ -21,63 +28,123 @@ class StorageBase {
         return path.join(year, month);
     }
 
-    /**
-     * 
-     * @param {String} dir 
-     * @param {String} name 
-     * @param {String} ext
-     * @param {Number} i index
+    /** Generate a filename with the following format: my-file-1a2b3c4d5e6f_o.png, with a filename length under MAX_FILENAME_LENGTH
+     *
+     * @param {String} stem -- stem of the file without path nor extension, e.g. my-file. If needed, this will be truncated, so that the filename is under MAX_FILENAME_BYTES
+     * @param {String} hash -- a secured hash to append the file, after the stem of the file, e.g. 1a2b3c4d5e6f
+     * @param {String} ext -- the extension of the file, e.g. ".png"
+     * @param {String} suffix -- optional, suffix to keep at the end of the stem, e.g. "_o"
+     * @returns {String}
+     */
+    generateFilename({stem, hash, suffix = '', ext = ''}) {
+        const encoder = new TextEncoder();
+        let filename = `${stem}-${hash}${suffix}${ext}`;
+
+        const fileNameBytes = encoder.encode(filename);
+
+        // If the filename has more bytes than the maximum allowed, truncate the stem
+        if (fileNameBytes.length > MAX_FILENAME_BYTES) {
+            const stemBytes = encoder.encode(stem);
+            const bytesToRemove = fileNameBytes.length - MAX_FILENAME_BYTES;
+            const newStemBytes = stemBytes.slice(0, -bytesToRemove);
+
+            const decoder = new TextDecoder();
+            filename = `${decoder.decode(newStemBytes)}-${hash}${suffix}${ext}`;
+        }
+
+        return filename;
+    }
+
+    /** Generate a secure hash for the filename, to make it very unlikely to be guessed and very likely to be unique
+     *  Uses 8 random bytes -> 8 * 8 = 64 bits of entropy -> 2^64 possible combinations (18 quintillion, i.e. 18 followed by 18 zeros)
+     *
+     *  @returns {String}
+     */
+    generateSecureHash() {
+        return crypto.randomBytes(8).toString('hex');
+    }
+
+    /** Generate a unique and secure filename
+     *
+     * @param {String} stem -- stem of the file without path nor extension, e.g. my-file
+     * @param {String} ext -- the extension of the file, e.g. ".png"
+     * @param {String} suffix -- optional, suffix to keep at the end of the stem, e.g. "_o"
      * @returns {Promise<String>}
      */
-    generateUnique(dir, name, ext, i) {
-        let filename,
-            append = '';
+    generateUniqueFilename({stem, suffix, ext}) {
+        const hash = this.generateSecureHash();
 
-        if (i) {
-            append = '-' + i;
-        }
-
-        if (ext) {
-            filename = name + append + ext;
-        } else {
-            filename = name + append;
-        }
-
-        return this.exists(filename, dir).then((exists) => {
-            if (exists) {
-                i = i + 1;
-                return this.generateUnique(dir, name, ext, i);
-            } else {
-                return path.join(dir, filename);
-            }
-        });
+        return this.generateFilename({stem, hash, suffix, ext});
     }
 
     /**
-     * @param {Object} file
-     * @param {String} file.name
-     * @param {String} targetDir
-     * 
-     * @returns {Promise<String>} unique file path
+     * @param {Object} options
+     * @param {Object} options.file
+     * @param {String} options.file.name -- the name of the file
+     * @param {String} options.file.suffix -- the suffix to use for the file, e.g. "_o"
+     * @param {String} options.targetDir -- the target directory to save the file in
+     * @returns {string} Generated unique file pathname
      */
-    getUniqueFileName(file, targetDir) {
-        var ext = path.extname(file.name), name;
+    getUniquePathname({
+        file: {
+            name: fileName,
+            suffix: fileSuffix = ORIGINAL_SUFFIX
+        },
+        targetDir
+    }) {
+        const basename = this.sanitizeBasename(path.basename(fileName));
+        const ext = this.getFileExtension(basename);
+        const suffix = this.getSuffix(basename, ext, fileSuffix);
+        const stem = path.basename(basename, suffix + ext);
 
-        // poor extension validation
-        // .1 or .342 is not a valid extension, .mp4 is though!
-        if (!ext.match(/\.\d+$/)) {
-            name = this.getSanitizedFileName(path.basename(file.name, ext));
-            return this.generateUnique(targetDir, name, ext, 0);
-        } else {
-            name = this.getSanitizedFileName(path.basename(file.name));
-            return this.generateUnique(targetDir, name, null, 0);
-        }
+        const filename = this.generateUniqueFilename({stem, suffix, ext});
+
+        return path.join(targetDir, filename);
     }
 
-    getSanitizedFileName(fileName) {
-        // below only matches ascii characters, @, and .
-        // unicode filenames like город.zip would therefore resolve to ----.zip
-        return fileName.replace(/[^\w@.]/gi, '-');
+    /** Extracts the file extension from the filename, if it is a valid extension
+     *
+     * @param {string} fileName
+     * @returns {string}
+     */
+    getFileExtension(filename) {
+        const ext = path.extname(filename);
+
+        // If the extension is not a valid extension, return an empty string
+        if (!ext.match(/(\.[a-z0-9]{2,10})+$/i)) {
+            return '';
+        }
+
+        return ext;
+    }
+
+    /** Extracts the given from the basename, if it exists
+     *
+     * @param {String} filename
+     * @param {String} ext
+     * @param {String} suffix
+     * @returns {String}
+     */
+    getSuffix(filename, ext, suffix) {
+        const stem = path.basename(filename, ext);
+
+        // If the stem does not end with the suffix, return an empty string
+        if (!stem.endsWith(suffix)) {
+            return '';
+        }
+
+        return suffix;
+    }
+
+    /** Sanitizes the basename of the file, to accept only ASCII characters, '@', and '.'
+     *  Replaces other characters with dashes.
+     *  Example: город.zip is replaced with ----.zip
+     *
+     * @param {String} basename
+     * @returns {String}
+     */
+    sanitizeBasename(basename) {
+        return basename.replace(/[^\w@.]/gi, '-');
     }
 }
 
--- test/BaseStorage.test.js
+++ test/BaseStorage.test.js
@@ -1,91 +1,120 @@
-const should = require('should'),
-    Promise = require('bluebird'),
-    StorageBase = require('../BaseStorage');
+const should = require('should');
+const Promise = require('bluebird');
+const StorageBase = require('../BaseStorage');
+const assert = require('assert').strict;
+
+const MAX_FILENAME_BYTES = 255;
 
 describe('Storage Base', function () {
-    it('getSanitizedFileName: escape non accepted characters in filenames', function () {
-        const storage = new StorageBase();
-        storage.getSanitizedFileName('(abc*@#123).zip').should.eql('-abc-@-123-.zip');
+    describe('sanitizeBasename', function () {
+        it('sanitizeBasename: escape non accepted characters in filenames', function () {
+            const storage = new StorageBase();
+            storage.sanitizeBasename('(abc*@#123).zip').should.eql('-abc-@-123-.zip');
+        });
     });
 
-    it('getUniqueFileName: accepts jpg', function (done) {
-        const storage = new StorageBase();
-        let i = 0;
+    describe('getUniquePathname', function () {
+        it('accepts jpg', function () {
+            const storage = new StorageBase();
+            const pathname = storage.getUniquePathname({file: {name: 'something.jpg'}, targetDir: 'target-dir'});
 
-        storage.exists = function () {
-            i = i + 1;
+            assert.match(pathname, /target-dir\/something-\w{16}\.jpg/);
+        });
 
-            if (i < 2) {
-                return Promise.resolve(true);
-            } else {
-                return Promise.resolve(false);
-            }
-        };
+        it('accepts png', function () {
+            const storage = new StorageBase();
+            const pathname = storage.getUniquePathname({file: {name: 'something.png'}, targetDir: 'target-dir'});
 
-        storage.getUniqueFileName({name: 'something.jpg'}, 'target-dir')
-            .then(function (filename) {
-                filename.should.eql('target-dir/something-1.jpg');
-                done();
-            })
-            .catch(done);
+            assert.match(pathname, /target-dir\/something-\w{16}\.png/);
+        });
+
+        it('accepts mp4', function () {
+            const storage = new StorageBase();
+            const pathname = storage.getUniquePathname({file: {name: 'something.mp4'}, targetDir: 'target-dir'});
+
+            assert.match(pathname, /target-dir\/something-\w{16}\.mp4/);
+        });
+
+        it('ignores invalid extension .1', function () {
+            const storage = new StorageBase();
+            const pathname = storage.getUniquePathname({file: {name: 'something.1'}, targetDir: 'target-dir'});
+
+            assert.match(pathname, /target-dir\/something.1-\w{16}/);
+        });
     });
 
-    it('getUniqueFileName: accepts png', function (done) {
-        const storage = new StorageBase();
+    describe('generateSecureHash', function () {
+        it('should return a 16 character hash', function () {
+            const storage = new StorageBase();
+            assert.equal(storage.generateSecureHash().length, 16);
+        });
 
-        storage.exists = function () {
-            return Promise.resolve(false);
-        };
-
-        storage.getUniqueFileName({name: 'something.png'}, 'target-dir')
-            .then(function (filename) {
-                filename.should.eql('target-dir/something.png');
-                done();
-            })
-            .catch(done);
+        it('should return a different hash for each call', function () {
+            const storage = new StorageBase();
+            assert.notEqual(storage.generateSecureHash(), storage.generateSecureHash());
+        });
     });
 
-    it('getUniqueFileName: accepts mp4', function (done) {
-        const storage = new StorageBase();
-        let i = 0;
+    describe('generateFilename', function () {
+        it('should return a filename with the original name, a secured hash, suffix and extension', function () {
+            const storage = new StorageBase();
+            const hash = 'a1b2c3d4e5f6';
+            const stem = 'something';
+            const suffix = '_o';
+            const ext = '.jpg';
 
-        storage.exists = function () {
-            i = i + 1;
+            assert.equal(storage.generateFilename({stem, hash, suffix, ext}), `${stem}-${hash}${suffix}${ext}`);
+        });
 
-            if (i < 2) {
-                return Promise.resolve(true);
-            } else {
-                return Promise.resolve(false);
-            }
-        };
+        it('should return a filename with the original name, a secured hash and suffix, when used without an extension', function () {
+            const storage = new StorageBase();
+            const hash = 'a1b2c3d4e5f6';
+            const stem = 'something';
+            const suffix = '_o';
 
-        storage.getUniqueFileName({name: 'something.mp4'}, 'target-dir')
-            .then(function (filename) {
-                filename.should.eql('target-dir/something-1.mp4');
-                done();
-            })
-            .catch(done);
-    });
+            assert.equal(storage.generateFilename({stem, hash, suffix}), `${stem}-${hash}${suffix}`);
+        });
 
-    it('getUniqueFileName: deny .1 extension', function (done) {
-        const storage = new StorageBase();
-        let i = 0;
+        it('should return a filename with the original name, a secured hash and extension, when used without a suffix', function () {
+            const storage = new StorageBase();
+            const hash = 'a1b2c3d4e5f6';
+            const stem = 'something';
+            const ext = '.jpg';
 
-        storage.exists = function () {
-            i = i + 1;
+            assert.equal(storage.generateFilename({stem, hash, ext}), `${stem}-${hash}${ext}`);
+        });
 
-            if (i < 2) {
-                return Promise.resolve(true);
-            } else {
-                return Promise.resolve(false);
-            }
-        };
+        it('should return a filename with the original name and a secured hash, when used without a suffix and extension', function () {
+            const storage = new StorageBase();
+            const hash = 'a1b2c3d4e5f6';
+            const stem = 'something';
 
-        storage.getUniqueFileName({name: 'something.1'}, 'target-dir')
-            .then(function (filename) {
-                filename.should.eql('target-dir/something.1-1');
-                done();
-            })
-            .catch(done);
+            assert.equal(storage.generateFilename({stem, hash}), `${stem}-${hash}`);
+        });
+
+        it('should truncate the basename to be under MAX_FILENAME_BYTES', function () {
+            const storage = new StorageBase();
+            const hash = 'a1b2c3d4e5f6';
+            const suffix = '_o';
+            const ext = '.jpg';
+            const lengthToRemove = '-'.length + hash.length + suffix.length + ext.length;
+            const stem = 'a'.repeat(MAX_FILENAME_BYTES);
+            const truncatedStem = 'a'.repeat(MAX_FILENAME_BYTES - lengthToRemove);
+
+            assert.equal(storage.generateFilename({stem, hash, suffix, ext}), `${truncatedStem}-${hash}${suffix}${ext}`);
+        });
+
+        it('should truncate a multi-bytes basename to be under MAX_FILENAME_BYTES', function () {
+            const storage = new StorageBase();
+            const hash = 'a1b2c3d4e5f6';
+            const suffix = '_o';
+            const ext = '.jpg';
+            const lengthToRemove = '-'.length + hash.length + suffix.length + ext.length;
+
+            const stem = '🌴'.repeat(100); // 100 🌴 characters = 100 * 4 bytes = 400 bytes
+            const truncatedStem = '🌴'.repeat((MAX_FILENAME_BYTES - lengthToRemove) / 4);
+
+            assert.equal(storage.generateFilename({stem, hash, suffix, ext}), `${truncatedStem}-${hash}${suffix}${ext}`);
+        });
     });
 });
