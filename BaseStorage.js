1c18f1f29ca819cba2c8bcc1fb0e5633fe25726d
Sag <guptazy@gmail.com>
2025-01-29 18:21:25 +0700

    🔒 Made filename generation more secure, performant and resilient to filesystems' limits (#70)
    ref https://linear.app/ghost/issue/ENG-1260
    ref https://linear.app/ghost/issue/ENG-1859
    
    - filenames are now generated using a 16-character hash, that gets re-generated on every upload
    - we do not check whether the file exists anymore, as the chance of a collision is near zero below a million files in a directory
    - if the filename length is higher than what most filesystem accept (255 bytes), we truncate the base of the filename, while keeping space for the unique hash and the file extension. Example to illustrate (non-accurate):
        - input: `long-name-for-an-image-imagine-255bytes-here-blablablablabla-and-a-bit-more.png`
        - output: `long-name-for-an-image-1a2b3c4d5e6f7g890.png`

--- BaseStorage.js
+++ BaseStorage.js
@@ -1,5 +1,10 @@
-const moment = require('moment'),
-    path = require('path');
+const moment = require('moment');
+const path = require('path');
+const crypto = require('crypto');
+
+// Most UNIX filesystems have a 255 bytes limit for the filename length
+// We keep 2 additional bytes, to make space for a file suffix (e.g. "_o" for original files after transformation)
+const MAX_FILENAME_BYTES = 253;
 
 class StorageBase {
     constructor() {
@@ -9,10 +14,16 @@ class StorageBase {
         });
     }
 
+    /**
+     * Returns the target directory for a given base directory
+     *
+     * @param {String} baseDir -- the base directory to save the file in
+     * @returns {string} -- Returns a directory path with the format baseDir/YYYY/MM, e.g. content/images/2025/01
+     */
     getTargetDir(baseDir) {
-        const date = moment(),
-            month = date.format('MM'),
-            year = date.format('YYYY');
+        const date = moment();
+        const month = date.format('MM');
+        const year = date.format('YYYY');
 
         if (baseDir) {
             return path.join(baseDir, year, month);
@@ -22,16 +33,129 @@ class StorageBase {
     }
 
     /**
-     * 
-     * @param {String} dir 
-     * @param {String} name 
+     * Generates a unique and secure file path for a given file and target directory
+     *
+     * @param {Object} file
+     * @param {String} file.name -- the name of the file
+     * @param {String} targetDir -- the target directory to save the file in
+     * @returns {string}
+     */
+    getUniqueSecureFilePath(file, targetDir) {
+        const originalFileName = path.basename(file.name);
+        const sanitizedFileName = this.sanitizeFileName(originalFileName);
+
+        const ext = this.getFileExtension(sanitizedFileName); // e.g. ".png"
+        const name = path.basename(sanitizedFileName, ext); // e.g. my-file
+        const hash = this.generateSecureHash(); // e.g. 1a2b3c4d5e6f7890
+
+        const newFileName = this.generateFileName({name, hash, ext}); // e.g. my-file-1a2b3c4d5e6f7890.png
+
+        return path.join(targetDir, newFileName);
+    }
+
+    /**
+     * Sanitizes the file name, to accept only ASCII characters, '@', and '.'
+     * Replaces other characters with dashes.
+     * Example: город.zip is replaced with ----.zip
+     *
+     * @param {String} fileName
+     * @returns {String}
+     */
+    sanitizeFileName(fileName) {
+        return fileName.replace(/[^\w@.]/gi, '-');
+    }
+
+    /**
+     * Extracts the file extension from the filename, if it is a valid extension
+     *
+     * @param {string} fileName
+     * @returns {string}
+     */
+    getFileExtension(filename) {
+        const ext = path.extname(filename);
+
+        // If the extension is not a valid extension, return an empty string
+        if (!ext.match(/(\.[a-z0-9]{2,10})+$/i)) {
+            return '';
+        }
+
+        return ext;
+    }
+
+    /**
+     * Generates a secure hash for the filename, to make it very unlikely to be guessed and very likely to be unique
+     * Uses 8 random bytes -> 8 * 8 = 64 bits of entropy -> 2^64 possible combinations (18 quintillion, i.e. 18 followed by 18 zeros)
+     *
+     * @returns {string}
+     */
+    generateSecureHash() {
+        return crypto.randomBytes(8).toString('hex');
+    }
+
+        /**
+     * Generates a filename with the following format: my-file-1a2b3c4d5e6f7890.png, with a filename length under MAX_FILENAME_LENGTH
+     *
+     * @param {String} name -- name of the file without extension, e.g. my-file. If needed, this will be truncated, so that the filename is under MAX_FILENAME_BYTES
+     * @param {String} hash -- a secured hash to append the file, after the stem of the file, e.g. 1a2b3c4d5e6f
+     * @param {String} ext -- the extension of the file, e.g. ".png"
+     * @returns {String}
+     */
+    generateFileName({name, hash, ext = ''}) {
+        const encoder = new TextEncoder();
+        let filename = `${name}-${hash}${ext}`;
+
+        const fileNameBytes = encoder.encode(filename);
+
+        // If the filename has more bytes than the maximum allowed, truncate the name
+        if (fileNameBytes.length > MAX_FILENAME_BYTES) {
+            const nameBytes = encoder.encode(name);
+            const bytesToRemove = fileNameBytes.length - MAX_FILENAME_BYTES;
+            const truncatedNameBytes = nameBytes.slice(0, -bytesToRemove);
+
+            const decoder = new TextDecoder();
+            filename = `${decoder.decode(truncatedNameBytes)}-${hash}${ext}`;
+        }
+
+        return filename;
+    }
+
+    /**
+     * [Deprecated] Returns a unique file path for a given file and target directory
+     *
+     * @param {Object} file
+     * @param {string} file.name
+     * @param {string} targetDir
+     * @returns {Promise<string>}
+     * @deprecated use getUniqueSecureFilePath instead
+     */
+    getUniqueFileName(file, targetDir) {
+        logging.warn('getUniqueFileName is deprecated. Use getUniqueSecureFilePath instead.');
+
+        var ext = path.extname(file.name), name;
+
+        // poor extension validation
+        // .1 or .342 is not a valid extension, .mp4 is though!
+        if (!ext.match(/\.\d+$/)) {
+            name = this.sanitizeFileName(path.basename(file.name, ext));
+            return this.generateUnique(targetDir, name, ext, 0);
+        } else {
+            name = this.sanitizeFileName(path.basename(file.name));
+            return this.generateUnique(targetDir, name, null, 0);
+        }
+    }
+
+    /**
+     * [Deprecated] Generates a unique file path using a numbering system, e.g. image.png, image-1.png, image-2.png, etc.
+     * @param {String} dir
+     * @param {String} name
      * @param {String} ext
      * @param {Number} i index
      * @returns {Promise<String>}
+     * @deprecated use getUniqueSecureFilePath instead
      */
     generateUnique(dir, name, ext, i) {
-        let filename,
-            append = '';
+        let filename;
+        let append = '';
 
         if (i) {
             append = '-' + i;
@@ -51,33 +175,6 @@ class StorageBase {
                 return path.join(dir, filename);
             }
         });
-    }
-
-    /**
-     * @param {Object} file
-     * @param {String} file.name
-     * @param {String} targetDir
-     * 
-     * @returns {Promise<String>} unique file path
-     */
-    getUniqueFileName(file, targetDir) {
-        var ext = path.extname(file.name), name;
-
-        // poor extension validation
-        // .1 or .342 is not a valid extension, .mp4 is though!
-        if (!ext.match(/\.\d+$/)) {
-            name = this.getSanitizedFileName(path.basename(file.name, ext));
-            return this.generateUnique(targetDir, name, ext, 0);
-        } else {
-            name = this.getSanitizedFileName(path.basename(file.name));
-            return this.generateUnique(targetDir, name, null, 0);
-        }
-    }
-
-    getSanitizedFileName(fileName) {
-        // below only matches ascii characters, @, and .
-        // unicode filenames like город.zip would therefore resolve to ----.zip
-        return fileName.replace(/[^\w@.]/gi, '-');
     }
 }
 
